1df64fcbc74d4650a3005703d2da4f77ac42e3d7
Luiza Petrinina <petrinina95@gmail.com>
2025-12-17 17:54:28 +0200

    Create a RootLayout
    Change the title and add a logo

--- the-wild-oasis-website/app/page.js
+++ the-wild-oasis-website/app/page.js
@@ -1,10 +1,9 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Home() {
     return (
         <div>
-            <Navigation />
+
             <h1>Home page</h1>
 
             <Link href="/cabins"> Explore luxury cabins!</Link>
