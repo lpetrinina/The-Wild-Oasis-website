b355a0bf9ceb695610c294edea0b1885563c737d
Luiza Petrinina <petrinina95@gmail.com>
2025-12-29 17:16:28 +0200

    Add main font

--- the-wild-oasis-website/src/app/layout.js
+++ the-wild-oasis-website/src/app/layout.js
@@ -1,7 +1,13 @@
 import Logo from "@/src/components/Logo";
 import Navigation from "@/src/components/Navigation";
 
-import "@/src/styles/globals.css"
+import "@/src/styles/globals.css";
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+    subsets: ['latin'],
+    display: 'swap'
+});
 
 
 export const metadata = {
@@ -17,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang='en'>
-            <body className="bg-primary-900 text-gray-50 min-h-screen">
+            <body className={`${josefin.className} bg-primary-900 text-gray-50 min-h-screen`}>
                 <header>
                     <Logo />
                     <Navigation />
