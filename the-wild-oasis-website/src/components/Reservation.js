23e050ac308401cd6a0e7146a30da4105b128d82
Luiza Petrinina <petrinina95@gmail.com>
2026-01-22 16:19:12 +0200

    Show the Reservation form if there is a loged user

--- the-wild-oasis-website/src/components/Reservation.js
+++ the-wild-oasis-website/src/components/Reservation.js
@@ -1,17 +1,30 @@
-import { getBookedDatesByCabinId, getSettings } from "../lib/data-service"
-import DateSelector from "./DateSelector"
-import ReservationForm from "./ReservationForm"
+import { auth } from "../lib/auth";
+
+import { getBookedDatesByCabinId, getSettings } from "../lib/data-service";
+
+import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
+import ReservationForm from "./ReservationForm";
 
 async function Reservation({ cabin }) {
+    const [settings, bookedDates] = await Promise.all([
+        getSettings(),
+        getBookedDatesByCabinId(cabin.id),
+    ]);
 
-    const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)])
+    const session = await auth();
 
     return (
-        <div className="grid grid-cols-1 max-h-[400] lg:grid-cols-2 border border-primary-800">
-            <DateSelector cabin={cabin} settings={settings} bookedDates={bookedDates} />
-            <ReservationForm cabin={cabin} />
+        <div className='grid grid-cols-1 max-h-[400] lg:grid-cols-2 border border-primary-800'>
+            <DateSelector
+                cabin={cabin}
+                settings={settings}
+                bookedDates={bookedDates}
+            />
+
+            {session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
         </div>
-    )
+    );
 }
 
 export default Reservation;
