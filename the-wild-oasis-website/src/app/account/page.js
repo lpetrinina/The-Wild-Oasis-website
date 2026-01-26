dabe5c4bcfe6773c3433813d79a89dcccb64c882
Luiza Petrinina <petrinina95@gmail.com>
2026-01-26 13:05:28 +0200

    Show user name on Account page

--- the-wild-oasis-website/src/app/account/page.js
+++ the-wild-oasis-website/src/app/account/page.js
@@ -1,7 +1,16 @@
-export const metadata = {
-    title: 'Guest area'
-}
+import { auth } from "@/src/lib/auth";
 
-export default function AccountPage() {
-    return <h3>Account Page</h3>
+
+export const metadata = {
+    title: "Guest area",
+};
+
+export default async function AccountPage() {
+
+    const session = await auth();
+    const firstName = session.user.name.split(' ').at(0);
+
+    return (
+        <h2 className='font-semibold text-2xl text-accent-400'> Welcome, {firstName}</h2>
+    );
 }
