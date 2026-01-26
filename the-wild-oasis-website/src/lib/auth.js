a3b45868d51c51b0182932de7f0b8dab9454306c
Luiza Petrinina <petrinina95@gmail.com>
2026-01-26 13:00:04 +0200

    Built a Sign In page

--- the-wild-oasis-website/src/lib/auth.js
+++ the-wild-oasis-website/src/lib/auth.js
@@ -12,7 +12,10 @@ const authConfig = {
         authorized({ auth, request }) {
             return !!auth?.user;
         }
+    },
+    pages: {
+        signIn: '/login'
     }
 };
 
-export const { auth, handlers: { GET, POST } } = NextAuth(authConfig)
+export const { auth, signIn, signOut, handlers: { GET, POST } } = NextAuth(authConfig);
