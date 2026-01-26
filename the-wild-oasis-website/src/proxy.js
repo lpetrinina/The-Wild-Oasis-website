9fafaf8918aabd9b667468d3a52c13f277bb19f5
Luiza Petrinina <petrinina95@gmail.com>
2026-01-26 13:04:23 +0200

    Migrate to proxy

--- the-wild-oasis-website/src/proxy.js
+++ the-wild-oasis-website/src/proxy.js
@@ -2,8 +2,7 @@ import { NextResponse } from "next/server";
 
 import { auth } from "./lib/auth";
 
-export const middleware = auth;
-
+export const proxy = auth;
 
 // middleware will run only for these route/s
 export const config = {
