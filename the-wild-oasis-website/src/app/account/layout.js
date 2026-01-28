3bdd24c9e69411e6444c564fcc0e8f6177412a05
Luiza Petrinina <petrinina95@gmail.com>
2026-01-28 18:13:50 +0200

    Remore the scroll bar

--- the-wild-oasis-website/src/app/account/layout.js
+++ the-wild-oasis-website/src/app/account/layout.js
@@ -1,7 +1,7 @@
 import SideNavigation from "@/src/components/SideNavigation";
 
 export default function Layout({ children }) {
-    return <div className="grid grid-cols-[16rem_1fr] gap-12 min-h-screen">
+    return <div className="grid grid-cols-[16rem_1fr] gap-12 h-full">
 
         <SideNavigation />
 
