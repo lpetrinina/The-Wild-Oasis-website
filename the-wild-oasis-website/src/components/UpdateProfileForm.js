28ac6450e1d4a23341a015df188e80c7411174bc
Luiza Petrinina <petrinina95@gmail.com>
2026-01-30 17:18:11 +0200

    Display a loading indicator
    Show the user that the form is being submitted

--- the-wild-oasis-website/src/components/UpdateProfileForm.js
+++ the-wild-oasis-website/src/components/UpdateProfileForm.js
@@ -1,42 +1,43 @@
-'use client'
+"use client";
 
+import { useFormStatus } from "react-dom";
 import { updateProfile } from "../lib/actions";
 
-
 function UpdateProfileForm({ guest, children }) {
-
     const { fullName, email, nationalID, countryFlag } = guest;
 
-
     return (
-        <form action={updateProfile} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col" >
-            <div className="space-y-2">
+        <form
+            action={updateProfile}
+            className='bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col'
+        >
+            <div className='space-y-2'>
                 <label>Full name</label>
                 <input
                     disabled
-                    name="fullName"
+                    name='fullName'
                     defaultValue={fullName}
-                    className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+                    className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
                 />
             </div>
 
-            <div className="space-y-2">
+            <div className='space-y-2'>
                 <label>Email address</label>
                 <input
                     disabled
-                    name="email"
+                    name='email'
                     defaultValue={email}
-                    className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+                    className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400'
                 />
             </div>
 
-            <div className="space-y-2">
-                <div className="flex items-center justify-between">
-                    <label htmlFor="nationality">Where are you from?</label>
+            <div className='space-y-2'>
+                <div className='flex items-center justify-between'>
+                    <label htmlFor='nationality'>Where are you from?</label>
                     <img
                         src={countryFlag}
-                        alt="Country flag"
-                        className="h-5 rounded-sm"
+                        alt='Country flag'
+                        className='h-5 rounded-sm'
                     />
                 </div>
 
@@ -44,22 +45,33 @@ function UpdateProfileForm({ guest, children }) {
                 {children}
             </div>
 
-            <div className="space-y-2">
-                <label htmlFor="nationalID">National ID number</label>
+            <div className='space-y-2'>
+                <label htmlFor='nationalID'>National ID number</label>
                 <input
-                    name="nationalID"
+                    name='nationalID'
                     defaultValue={nationalID}
-                    className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+                    className='px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm'
                 />
             </div>
 
-            <div className="flex justify-end items-center gap-6">
-                <button className=" cursor-pointer bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-                    Update profile
-                </button>
+            <div className='flex justify-end items-center gap-6'>
+                <SubmitButton />
             </div>
         </form>
-    )
+    );
 }
 
-export default UpdateProfileForm
+function SubmitButton() {
+    const { pending } = useFormStatus();
+
+    return (
+        <button
+            className=' cursor-pointer bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'
+            disabled={pending}
+        >
+            {pending ? "Updating..." : "Update profile"}
+        </button>
+    );
+}
+
+export default UpdateProfileForm;
