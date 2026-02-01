78e205b8d9261d31cb4c1bd8a430c6308a0f849e
Luiza Petrinina <petrinina95@gmail.com>
2026-02-01 19:00:08 +0200

    Show loading indicator to a user while the reservation is deleting

--- the-wild-oasis-website/src/components/DeleteReservation.js
+++ the-wild-oasis-website/src/components/DeleteReservation.js
@@ -1,13 +1,33 @@
 'use client';
 
+import { useTransition } from 'react';
 import { TrashIcon } from '@heroicons/react/24/solid';
+
 import { deleteReservation } from '../lib/actions';
+import SpinnerMini from './SpinnerMini';
 
 function DeleteReservation({ bookingId }) {
+
+    const [isPending, startTransition] = useTransition();
+
+    const handleDelete = function () {
+        const isConfirmed = confirm('Are you sure you want to delete this reservation?');
+
+        if (isConfirmed) {
+            startTransition(() => deleteReservation(bookingId));
+        };
+    }
+
+
     return (
-        <button onClick={() => deleteReservation(bookingId)} className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
-            <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-            <span className='mt-1'>Delete</span>
+        <button onClick={handleDelete} className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
+            {!isPending ?
+                <>
+                    <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
+                    <span className='mt-1'>Delete</span>
+                </>
+                : <span className='mx-auto'> <SpinnerMini /></span>
+            }
         </button>
     );
 }
