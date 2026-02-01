fda85460d30047987143f97453e0373f93e95b0d
Luiza Petrinina <petrinina95@gmail.com>
2026-02-01 13:51:12 +0200

    Implement delete reservation functionality

--- the-wild-oasis-website/src/components/ReservationCard.js
+++ the-wild-oasis-website/src/components/ReservationCard.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { PencilSquareIcon } from '@heroicons/react/24/solid';
 import { format, formatDistance, isPast, isToday, parseISO } from 'date-fns';
 
-// import DeleteReservation from './DeleteReservation';
+import DeleteReservation from './DeleteReservation';
 
 export const formatDistanceFromNow = (dateStr) =>
     formatDistance(parseISO(dateStr), new Date(), {
@@ -74,19 +74,24 @@ function ReservationCard({ booking }) {
 
             {/* Available for upcoming bookings */}
             <div className='flex flex-col border-l border-primary-800 w-25'>
-                {/* EDIT */}
-                {/* <Link
-                    href={`/account/reservations/edit/${id}`}
-                    className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'
-                >
-                    <PencilSquareIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-                    <span className='mt-1'>Edit</span>
-                </Link> */}
 
-                {/* DELETE */}
-                {/* <DeleteReservation bookingId={id} /> */}
+                {!isPast(new Date(startDate)) ?
+                    <>
+                        <Link
+                            href={`/account/reservations/edit/${id}`}
+                            className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'
+                        >
+                            <PencilSquareIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
+                            <span className='mt-1'>Edit</span>
+                        </Link>
+
+
+                        <DeleteReservation bookingId={id} />
+                    </>
+                    : null
+                }
             </div>
-        </div>
+        </div >
     );
 }
 
--- the-wild-oasis-website/src/lib/actions.js
+++ the-wild-oasis-website/src/lib/actions.js
@@ -1,32 +1,32 @@
-'use server';
+"use server";
 
 import { supabase } from "./supabase";
 import { auth, signIn, signOut } from "./auth";
 import { refresh, revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
 export async function signInAction() {
-    await signIn('google', { redirectTo: '/account' });
-};
+    await signIn("google", { redirectTo: "/account" });
+}
 
 export async function signOutAction() {
-    await signOut({ redirectTo: '/' });
-};
+    await signOut({ redirectTo: "/" });
+}
 
 export async function updateProfile(formData) {
-
     // Check if there is an authorized user
     const session = await auth();
     if (!session) {
-        throw new Error('You must be logged in!')
+        throw new Error("You must be logged in!");
     }
 
-    const nationalID = formData.get('nationalID');
-    const [nationality, countryFlag] = formData.get('nationality').split('%');
+    const nationalID = formData.get("nationalID");
+    const [nationality, countryFlag] = formData.get("nationality").split("%");
 
     // Validate a nationalID
     const pattern = /^[a-zA-Z0-9]{6,12}$/;
     if (!pattern.test(nationalID)) {
-        throw new Error('Please provide a valid national ID!');
+        throw new Error("Please provide a valid national ID!");
     }
 
     const updatedData = { nationalID, nationality, countryFlag };
@@ -43,6 +43,36 @@ export async function updateProfile(formData) {
 
     // Revalidate cache
     refresh();
-    revalidatePath('/account/profile');
+    revalidatePath("/account/profile");
+}
 
+export async function deleteReservation(bookingId) {
+
+    // Check if there is an authorized user
+    const session = await auth();
+    if (!session) {
+        throw new Error("You must be logged in!");
+    }
+
+    // Check if the reservation is really made by the user
+    const guestBookings = await getBookings(session.user.guestId);
+    const guestBookingsIds = guestBookings.map(booking => booking.id);
+
+    if (!guestBookingsIds.includes(bookingId)) {
+        throw new Error('You are not allowed to delete this booking!')
+    }
+
+    // Delete booking in Supabase
+    const { error } = await supabase
+        .from("bookings")
+        .delete()
+        .eq("id", bookingId);
+
+    if (error) {
+        console.error(error);
+        throw new Error("Booking could not be deleted");
+    }
+
+    // Revalidate cache
+    revalidatePath("/account/reservations");
 }
