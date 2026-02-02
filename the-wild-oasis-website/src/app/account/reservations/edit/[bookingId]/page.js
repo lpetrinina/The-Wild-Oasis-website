1055002d548aeaefe6db1de0b898a767814a065f
Luiza Petrinina <petrinina95@gmail.com>
2026-02-02 22:33:52 +0200

    Extract the Submit button

--- the-wild-oasis-website/src/app/account/reservations/edit/[bookingId]/page.js
+++ the-wild-oasis-website/src/app/account/reservations/edit/[bookingId]/page.js
@@ -1,3 +1,4 @@
+import SubmitButton from "@/src/components/SubmitButton";
 import { updateReservation } from "@/src/lib/actions";
 import { getBooking, getCabin } from "@/src/lib/data-service";
 
@@ -55,9 +56,9 @@ async function Page({ params }) {
                 />
 
                 <div className='flex justify-end items-center gap-6'>
-                    <button className='cursor-pointer bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
+                    <SubmitButton>
                         Update reservation
-                    </button>
+                    </SubmitButton>
                 </div>
             </form>
         </div>
--- the-wild-oasis-website/src/components/UpdateProfileForm.js
+++ the-wild-oasis-website/src/components/UpdateProfileForm.js
@@ -1,7 +1,7 @@
 "use client";
 
-import { useFormStatus } from "react-dom";
 import { updateProfile } from "../lib/actions";
+import SubmitButton from "./SubmitButton";
 
 function UpdateProfileForm({ guest, children }) {
     const { fullName, email, nationalID, countryFlag } = guest;
@@ -55,23 +55,14 @@ function UpdateProfileForm({ guest, children }) {
             </div>
 
             <div className='flex justify-end items-center gap-6'>
-                <SubmitButton />
+                <SubmitButton>
+                    Update profile
+                </SubmitButton>
             </div>
         </form>
     );
 }
 
-function SubmitButton() {
-    const { pending } = useFormStatus();
-
-    return (
-        <button
-            className=' cursor-pointer bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'
-            disabled={pending}
-        >
-            {pending ? "Updating..." : "Update profile"}
-        </button>
-    );
-}
 
 export default UpdateProfileForm;
+
