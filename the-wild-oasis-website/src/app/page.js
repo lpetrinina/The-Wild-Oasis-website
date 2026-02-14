6908f8c6b1e0a14a542a33a11ae0b22154587e7e
Luiza Petrinina <petrinina95@gmail.com>
2026-02-14 18:02:32 +0200

    Make Home and Thank you page responsive

--- the-wild-oasis-website/src/app/page.js
+++ the-wild-oasis-website/src/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
             />
 
             <div className='relative z-10 text-center'>
-                <h1 className='text-8xl text-primary-50 mb-10 tracking-tight font-normal'>
+                <h1 className='text-6xl md:text-8xl text-primary-50 mb-10 tracking-tight font-normal'>
                     Welcome to paradise.
                 </h1>
 
