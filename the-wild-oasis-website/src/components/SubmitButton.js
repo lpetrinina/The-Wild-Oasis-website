a6539c0f8fc86ea10b17a1fca7a43a28d09f0487
Luiza Petrinina <petrinina95@gmail.com>
2026-02-15 12:20:12 +0200

    Fix the design of the Submit button and make it responsive

--- the-wild-oasis-website/src/components/SubmitButton.js
+++ the-wild-oasis-website/src/components/SubmitButton.js
@@ -9,7 +9,7 @@ function SubmitButton({ children, pendingLabel = 'Updating...', disabled }) {
     const isDisabled = pending || disabled;
 
     return (
-        <button className='cursor-pointer bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'
+        <button className='cursor-pointer bg-accent-500 px-6 py-3 sm:py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-accent-300 disabled:text-primary-600'
             disabled={isDisabled}
         >
             {pending ? pendingLabel : children}
