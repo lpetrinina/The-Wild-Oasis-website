be671375377c73ec8452ae0f612f435e4c1fc42a
Luiza Petrinina <petrinina95@gmail.com>
2026-02-16 18:04:07 +0200

    Make the Side navigation responsive

--- the-wild-oasis-website/src/app/account/layout.js
+++ the-wild-oasis-website/src/app/account/layout.js
@@ -1,7 +1,7 @@
 import SideNavigation from "@/src/components/SideNavigation";
 
 export default function Layout({ children }) {
-    return <div className="grid grid-cols-[16rem_1fr] gap-12 h-full">
+    return <div className="grid grid-cols-[3rem_1fr] lg:grid-cols-[14rem_1fr] gap-6 md:gap-8 lg:gap-12 h-full">
 
         <SideNavigation />
 
--- the-wild-oasis-website/src/components/SideNavigation.js
+++ the-wild-oasis-website/src/components/SideNavigation.js
@@ -1,29 +1,32 @@
-'use client';
+"use client";
 
 import {
     CalendarDaysIcon,
+    ChevronDoubleLeftIcon,
+    ChevronDoubleRightIcon,
     HomeIcon,
     UserIcon,
-} from '@heroicons/react/24/solid';
-import Link from 'next/link';
+} from "@heroicons/react/24/solid";
+import Link from "next/link";
 
-import SignOutButton from './SignOutButton';
-import { usePathname } from 'next/navigation';
+import SignOutButton from "./SignOutButton";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navLinks = [
     {
-        name: 'Home',
-        href: '/account',
+        name: "Home",
+        href: "/account",
         icon: <HomeIcon className='h-5 w-5 text-primary-600' />,
     },
     {
-        name: 'Reservations',
-        href: '/account/reservations',
+        name: "Reservations",
+        href: "/account/reservations",
         icon: <CalendarDaysIcon className='h-5 w-5 text-primary-600' />,
     },
     {
-        name: 'Guest profile',
-        href: '/account/profile',
+        name: "Guest profile",
+        href: "/account/profile",
         icon: <UserIcon className='h-5 w-5 text-primary-600' />,
     },
 ];
@@ -31,20 +34,25 @@ const navLinks = [
 function SideNavigation() {
     const pathname = usePathname();
 
+
     return (
-        <nav className='border-r border-primary-900'>
+
+        <nav
+            className={'h-full bg-primary-950  border-r border-primary-900'}
+        >
 
             <ul className='flex flex-col gap-2 h-full text-lg'>
-
                 {navLinks.map((link) => (
-
                     <li key={link.name}>
                         <Link
-                            className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold  ${pathname === link.href ? 'bg-primary-900' : ''}`}
+                            className={`py-3 px-3 lg:px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center sm:gap-4 font-semibold  
+                                ${pathname === link.href ? "bg-primary-900" : ""}`}
                             href={link.href}
                         >
                             {link.icon}
-                            <span>{link.name}</span>
+
+                            <span className="hidden lg:inline-block">{link.name}</span>
+
                         </Link>
                     </li>
                 ))}
@@ -54,6 +62,7 @@ function SideNavigation() {
                 </li>
             </ul>
         </nav>
+
     );
 }
 
